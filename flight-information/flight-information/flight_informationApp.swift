43e0c97ea7e01cb687c75d3fbaaf49a7c547813d
Dayeon Lee <dayeon2768@daum.net>
2023-09-05 08:09:42 +0900

    Chore: Add Project

---
+++ flight-information/flight-information/flight_informationApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_informationApp.swift
+//  flight-information
+//
+//  Created by 이다연 on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct flight_informationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flight-information/flight-informationUITests/flight_informationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  flight_informationUITestsLaunchTests.swift
+//  flight-informationUITests
+//
+//  Created by 이다연 on 2023/09/04.
+//
+
+import XCTest
+
+final class flight_informationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
