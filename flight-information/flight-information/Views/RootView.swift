aff51aceada08d028eb9831c4ac8bd8044e2f2b5
Dayeon Lee <dayeon2768@daum.net>
2023-09-06 10:35:09 +0900

    Feat: Add RootView

---
+++ flight-information/flight-information/Views/RootView.swift
@@ -0,0 +1,34 @@
+//
+//  RootView.swift
+//  flight-information
+//
+//  Created by 이다연 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @State private var selectedIndex: Int = 0
+    var body: some View {
+        TabView(selection: $selectedIndex) {
+            HomeView()
+                .tabItem({
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                })
+                .tag(0)
+            FavoritesView()
+                .tabItem({
+                    Image(systemName: "star.fill")
+                    Text("Favorite")
+                })
+                .tag(1)
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
