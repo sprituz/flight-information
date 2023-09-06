1f28070c0725c11ee1423fb673f8c02e0f570ff2
Dayeon Lee <dayeon2768@daum.net>
2023-09-06 10:39:25 +0900

    Feat: Create FavoritesView

---
+++ flight-information/flight-information/Views/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  flight-information
+//
+//  Created by 이다연 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("즐겨찾기뷰")
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
