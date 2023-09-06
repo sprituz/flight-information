d1bdc3b2eda8f5802bfd36293003fd3d0d432661
Dayeon Lee <dayeon2768@daum.net>
2023-09-06 16:29:24 +0900

    Feat: Creat ListView

---
+++ flight-information/flight-information/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  flight-information
+//
+//  Created by 이다연 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
