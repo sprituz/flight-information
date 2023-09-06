db0f4dc03611aafdfd051da3443dce895e524075
Dayeon Lee <dayeon2768@daum.net>
2023-09-06 16:29:12 +0900

    Feat: Search Flight info feature complete

--- flight-information/flight-information/Services/APIService.swift
+++ flight-information/flight-information/Services/APIService.swift
@@ -30,7 +30,7 @@ enum API{
     var url: URL{
         
         switch self {
-        case .getFlightOpratInfoList: return URL(string: "http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey=" + String(Bundle.main.FLIGHT_API_KEY) + "&pageNo=1&numOfRows=10000&_type=json&depAirportId=NAARKJJ&arrAirportId=NAARKPC&depPlandTime=20230403&airlineId=AAR")!
+        case .getFlightOpratInfoList: return URL(string: "http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList")!
         
         case .getArprtList: return URL(string: "http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getArprtList?serviceKey=" + String(Bundle.main.FLIGHT_API_KEY) + "&_type=json")!
             
@@ -43,8 +43,21 @@ enum ApiService {
     
     ///  항공운항정보 목록 가져오기
     /// - Returns: AnyPublisher<ArprtListResponse, Error>
-    static func getFlightOpratInfoList() -> AnyPublisher<FlightOpratInfoListResponse , Error> {
-        return AF.request(API.getFlightOpratInfoList.url)
+    static func getFlightOpratInfoList(depAirportId:String,
+                                       arrAirportId: String,
+                                       depPlandTime:String,
+                                       airlineId: String) -> AnyPublisher<FlightOpratInfoListResponse , Error> {
+        let queryItems = [
+            URLQueryItem(name: "serviceKey", value: String(Bundle.main.FLIGHT_API_KEY)),
+            URLQueryItem(name: "pageNo", value: "1"),
+            URLQueryItem(name: "numOfRows", value: "10000"),
+            URLQueryItem(name: "_type", value: "json"),
+            URLQueryItem(name: "depAirportId", value: depAirportId),
+                          URLQueryItem(name: "arrAirportId", value: arrAirportId),
+                         URLQueryItem(name: "depPlandTime", value: depPlandTime),
+                         URLQueryItem(name: "airlineId", value: airlineId)]
+        let newUrl = API.getFlightOpratInfoList.url.appending(queryItems: queryItems)
+        return AF.request(newUrl)
             .publishDecodable(type: FlightOpratInfoListResponse.self) //디코딩
             .value() // 값만 가져오기
             .mapError { (afError:AFError) in
