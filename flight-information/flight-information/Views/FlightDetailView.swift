d0bcbb8be0c02d68b5e352c32e2ce1249ff5d396
Dayeon Lee <dayeon2768@daum.net>
2023-09-08 13:33:56 +0900

    Feat: Add data into FlightDetailView

--- flight-information/flight-information/Views/FlightDetailView.swift
+++ flight-information/flight-information/Views/FlightDetailView.swift
@@ -11,15 +11,21 @@ struct FlightDetailView: View {
     let flightInfo: FlightOpratInfo
     
     var body: some View {
-        VStack{
-            Text("항공편명")
-            Text("항공사명")
-            Text("출발시간")
-            Text("도착시간")
-            Text("일반석운임")
-            Text("비즈니스석운임")
-            Text("출발공항")
-            Text("도착공항")
+        VStack(spacing: 15){
+            Text("항공편명: \(flightInfo.vihicleId)")
+            Text("항공사명: \(flightInfo.airlineNm)")
+            HStack {
+                Text("출발일:" + (String(flightInfo.depPlandTime).toDate()?.formatted("yyyy/MM/dd") ?? ""))
+                Text("출발시간:" + (String(flightInfo.depPlandTime).toDate()?.formatted(date: .omitted, time: .shortened) ?? ""))
+            }
+            HStack {
+                Text("도착일:" + (String(flightInfo.arrPlandTime).toDate()?.formatted("yyyy/MM/dd") ?? ""))
+                Text("도착시간:" + (String(flightInfo.arrPlandTime).toDate()?.formatted(date: .omitted, time: .shortened) ?? ""))
+            }
+            Text("일반석운임: \(flightInfo.economyCharge)원")
+            Text("비즈니스석운임: \(flightInfo.prestigeCharge)원")
+            Text("출발공항: \(flightInfo.depAirportNm)공항")
+            Text("도착공항: \(flightInfo.arrAirportNm)공항")
         }
     }
 }
